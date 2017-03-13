bd4529bff5090a0dda4069c0b0a7e963dd9c0c4f
lujun.chen <lujun.chen@msxf.com>
2017-03-13 14:18:10 +0800

    【约定优于配置】几次PageHellper分页插件

--- src/main/java/com/itclj/service/CityService.java
+++ src/main/java/com/itclj/service/CityService.java
@@ -1,6 +1,7 @@
 package com.itclj.service;
 
 import com.itclj.model.CityBean;
+import java.util.List;
 
 /**
  * Created by lujun.chen on 2017/3/13.
@@ -15,6 +16,16 @@ public interface CityService {
   /**
    * 按照ID获取
    */
-  public CityBean findById(Short id);
+  CityBean findById(Short id);
 
+
+  /**
+   * 分页查询
+   *
+   * @param cityBean 查询条件
+   * @param pageNum 页码
+   * @param pageSize 每页记录数
+   * @return 查询结果
+   */
+  List<CityBean> listPage(CityBean cityBean, Integer pageNum, Integer pageSize);
 }
--- src/test/java/com/itclj/service/CityServiceTest.java
+++ src/test/java/com/itclj/service/CityServiceTest.java
@@ -1,8 +1,12 @@
 package com.itclj.service;
 
+import com.alibaba.fastjson.JSON;
 import com.itclj.model.CityBean;
+import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,6 +17,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CityServiceTest {
+
+  private static Logger logger= LoggerFactory.getLogger(CityServiceTest.class);
 
   @Autowired
   private CityService cityService;
@@ -25,4 +31,14 @@ public class CityServiceTest {
     cityService.save(cityBean);
   }
 
+  @Test
+  public void listPage() {
+    Integer pageNum=1;//页码，从1开始
+    Integer pageSize=10;//每页记录数
+    CityBean cityBean = new CityBean();
+    //cityBean.setCity("Zaria");
+    List<CityBean> list = cityService.listPage(cityBean,pageNum,pageSize);
+    logger.info(JSON.toJSONString(list));
+
+  }
 }
